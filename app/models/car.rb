137a73f4d819411911ef291bdf4b5245e535a01e
Stefan Koch <webokoch@gmx.de>
2021-08-25 17:33:49 +0200

    added validations to car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,6 @@
 class Car < ApplicationRecord
+  validates :model, presence: true, uniqueness: true
+  validates :color, presence: true
+  validates :year, presence: true, numericality: { greater_than: 2000 }
+  validates :mileage, presence: true, numericality: { greater_than: 0 }
 end
